dd54d814f7ac186541a26e84911c56aaca4331a0
mhmdashraf11 <146073242+mhmdashraf11@users.noreply.github.com>
2024-03-27 22:17:32 +0200

    Update assignment3.cpp
    v3

--- assignment3.cpp
+++ assignment3.cpp
@@ -1,8 +1,6 @@
 
 #include <iostream>
 #include "Image_Class.h"
-#include <algorithm>
-#include <regex>
 using namespace std;
 bool isValidFilename(const string& filename) {   //from Image_class.h
     const char* extension = strrchr(filename.c_str(), '.');
@@ -13,11 +11,22 @@ bool isValidFilename(const string& filename) {   //from Image_class.h
 
     return true;
 }
-Image input(){
+
+Image input(){ // to take the image and check the validation of its name and extension
     string filename;
     cout << "Enter the image name to upload: ";
     cin >> filename;
-    Image newimg(filename);
+    while(!isValidFilename(filename)){
+        cin >> filename;
+    }
+    Image newimg;
+    try{
+        newimg.loadNewImage(filename);
+    }
+    catch(invalid_argument){
+        cout << "Please enter a valid image." <<endl;
+        input();
+    }
 
     return newimg;
 }
@@ -28,7 +37,7 @@ string SaveMenu(){
     cin >> choice;
     return choice;
 }
-void filter3(Image& img){
+void filter3(Image& img){ //invert image
 
     for(int i=0; i<img.width; i++){
         for(int j=0; j<img.height; j++){
@@ -37,8 +46,6 @@ void filter3(Image& img){
             img(i,j,2) = 255-img(i,j,2);
         }
     }
-    // img.saveImage("mario.jpg");
-
 }
 
 
@@ -71,20 +78,20 @@ void flip_horizontal(Image& image){
 }
 
 void choice_flip(Image& img){
-    cout << "What type of flip do you need?\n";
-    cout << "1)Flip vertical\n2)Flip horizontal\n";
-    string choice;
-    cin >> choice;
-    cin.ignore(); // Clear the newline character from the input buffer
+        cout << "What type of flip do you need?\n";
+        cout << "1)Flip vertical\n2)Flip horizontal\n";
+        string choice;
+        cin >> choice;
+        cin.ignore(); // Clear the newline character from the input buffer
 
-    if(choice == "1"){
-        flip_vertical(img);
-    }else if(choice == "2"){
-        flip_horizontal(img);
-    }else{
-        cout << "Invalid choice,Please enter a valid choice." << endl << endl;
-        return choice_flip(img);
-    }
+        if(choice == "1"){
+             flip_vertical(img);
+        }else if(choice == "2"){
+             flip_horizontal(img);
+        }else{
+            cout << "Invalid choice,Please enter a valid choice." << endl << endl;
+            return choice_flip(img);
+        }
 }
 
 void filter1(Image& image){ //Gray scale
@@ -142,18 +149,125 @@ void bandw(Image& image){ // Black and white
         }
     }
 }
+//void rotate270(Image& img) {
+//    int newWidth = img.height; // Swap width and height for a 270-degree rotation
+//    int newHeight = img.width;
+//    Image rotatedImage(newWidth, newHeight);
+//
+//    for (int y = 0; y < img.height; y++) {
+//        for (int x = 0; x < img.width; x++) {
+//            int newX = img.height - y - 1; // Adjusted coordinates for 270-degree rotation
+//            int newY = x;
+//            for (int k = 0; k < 3; k++) {
+//                rotatedImage.setPixel(newX, newY, k, img.getPixel(x, y, k));
+//            }
+//        }
+//    }
+//
+//    img = rotatedImage; // Store rotatedImage in img
+//}
+void rotate90(Image& img) {
+    // Swap width and height for a 270-degree rotation
+    int newWidth = img.height;
+    int newHeight = img.width;
+
+    // Create a temporary buffer to store pixel values during rotation
+    unsigned char* tempBuffer = new unsigned char[newWidth * newHeight * img.channels];
+
+    // Iterate over the image pixels and perform the rotation in-place
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            // Calculate the new coordinates after rotation
+            int newX = img.height - y - 1;
+            int newY = x;
+
+            // Copy pixel values to the temporary buffer
+            for (int k = 0; k < img.channels; k++) {
+                tempBuffer[(newY * newWidth + newX) * img.channels + k] = img.getPixel(x, y, k);
+            }
+        }
+    }
+
+    // Update the dimensions of the image
+    img.width = newWidth;
+    img.height = newHeight;
+
+    // Copy the rotated pixel values back to the image
+    for (int i = 0; i < newWidth * newHeight * img.channels; i++) {
+        img.imageData[i] = tempBuffer[i];
+    }
+
+    // Free the temporary buffer
+    delete[] tempBuffer;
+}
+
+
+
+void rotate180(Image& img) {
+    Image rotatedImage(img.width, img.height);
+    // Uncomment the line below if you want to initialize img with the correct dimensions
+    // img = Image(img.width, img.height);
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            int newX = img.width - x - 1;  // make the coordinates upside down
+            int newY = img.height - y - 1;
+            for (int k = 0; k < 3; k++) {
+                rotatedImage.setPixel(newX, newY, k, img.getPixel(x, y, k));
+            }
+        }
+    }
+
+    // Copy rotatedImage to img
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            for (int k = 0; k < 3; k++) {
+                img.setPixel(x, y, k, rotatedImage.getPixel(x, y, k));
+            }
+        }
+    }
+}
+void rotate270(Image& img){
+    rotate180(img);// to make the total rotate equals 270 degree
+    rotate90(img);
+}
+
+void rotateMenu(Image& img){
+    int choice;
+    cout << "Please choose the rotate angle:\n";
+    cout << "1. 90\n";
+    cout << "2. 180\n";
+    cout << "3. 270\n";
+    cin >> choice;
+    switch(choice){
+        case 1 :
+            rotate90(img);
+            break;
+        case 2 :
+            rotate180(img);
+            break;
+        case 3 :
+            rotate270(img);
+            break;
+        default :
+            cout << "Please enter a number from 1 to 3."<<endl;
+            rotateMenu(img);
+    }
+}
+
 
 
 void mainmenu(Image &image){
     string choice;
-    cout << "Choose the filter\n";
+    cout << "=>Choose the filter\n";
     cout << "1. Gray scale\n";
     cout << "2. Black and white\n";
     cout << "3. Invert image\n";
-    cout << "4. Merge images\n";
-    cout << "5. Flip image\n";
+    cout << "4. Flip image\n";
+    cout << "5. rotate image\n";
+    cout<< "6. exit\n";
     cin >> choice;
-    if(cin.fail() || (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "5")){
+    if(cin.fail() || (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "5" && choice !="6")){
         cout << "Please enter a number between 1 and 5\n";
         return mainmenu(image);
     }
@@ -167,7 +281,15 @@ void mainmenu(Image &image){
         filter3(image);
     }
     else if(choice == "5"){
+        rotateMenu( image);
+//        merge(image);
+    }
+    else if(choice == "4"){
         choice_flip(image);
+    }
+    else if(choice == "6"){
+        cout << "Thanks for your time!\n";
+        exit(0);
     }
     cout << "Do you want to apply another filter?\n";
     cout << "1. Yes\n2. No\n";
@@ -179,12 +301,11 @@ void mainmenu(Image &image){
     else if(choice4 == "2"){
         string choice2;
         choice2 = SaveMenu();
-        if(choice2 == "1"){
+        if(choice2 == "1" ){
             string ImageName;
             cout << "Enter the image name and its extension to save:\n";
             cout << "<e.x.ImageName.jpg>\n";
             cin >> ImageName;
-
             if(image.saveImage(ImageName)){
                 cout << "Image saved successfully\n";
             }
@@ -199,6 +320,13 @@ void mainmenu(Image &image){
             cout << "Invalid input. try again." << endl;
             while(choice2 != "1" && choice2 !="2"){
                 choice2 = SaveMenu();
+            }
+            if(choice2 == "1") {
+                string ImageName;
+                cout << "Enter the image name and its extension to save:\n";
+                cout << "<e.x.ImageName.jpg>\n";
+                cout << "*if you want save it in the same file enter the same image name*\n";
+                cin >> ImageName;
             }
         }
     }
