9612673dd257a770690c17a1a735af8d5b1633d1
mhmdashraf11 <146073242+mhmdashraf11@users.noreply.github.com>
2024-04-10 18:40:42 +0200

    Update blur.cpp

--- blur.cpp
+++ blur.cpp
@@ -1,37 +1,73 @@
 #include "Image_Class.h"
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-void Blur(Image& img){
-    int avgR, avgG, avgB;
-    for (int i = 21; i < img.width - 21; i++) {
-        for (int j = 21; j < img.height - 21; j++) {
-            int sumR = 0, sumG = 0, sumB = 0;
-            for (int m = -21; m <= 21; m++) {
-                for (int n = -21; n <= 21; n++) {
-                
-                    sumR += img(i + m, j + n, 0);
-                    sumG += img(i + m, j + n, 1);
-                    sumB += img(i + m, j + n, 2);
-                    
-                }
+void summed_table(Image& img, vector<vector<int>>& prefixSumR, vector<vector<int>>& prefixSumG, vector<vector<int>>& prefixSumB) {
+    int width = img.width;
+    int height = img.height;
+
+    prefixSumR.assign(width, vector<int>(height, 0));
+    prefixSumG.assign(width, vector<int>(height, 0));
+    prefixSumB.assign(width, vector<int>(height, 0));
+
+    // Compute prefix sums for each color channel separately
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            prefixSumR[i][j] = img(i, j, 0);
+            prefixSumG[i][j] = img(i, j, 1);
+            prefixSumB[i][j] = img(i, j, 2);
+
+            if (i > 0) {
+                prefixSumR[i][j] += prefixSumR[i - 1][j];
+                prefixSumG[i][j] += prefixSumG[i - 1][j];
+                prefixSumB[i][j] += prefixSumB[i - 1][j];
             }
-            avgR = sumR / 1849;
-            avgG = sumG / 1849;
-            avgB = sumB / 1849;
-            img(i, j, 0) = avgR;
-            img(i, j, 1) = avgG;
-            img(i, j, 2) = avgB;
+            if (j > 0) {
+                prefixSumR[i][j] += prefixSumR[i][j - 1];
+                prefixSumG[i][j] += prefixSumG[i][j - 1];
+                prefixSumB[i][j] += prefixSumB[i][j - 1];
+            }
+            if (i > 0 && j > 0) {
+                prefixSumR[i][j] -= prefixSumR[i - 1][j - 1];
+                prefixSumG[i][j] -= prefixSumG[i - 1][j - 1];
+                prefixSumB[i][j] -= prefixSumB[i - 1][j - 1];
+            }
         }
-        
     }
 }
 
-int main() {
-    Image img("building.jpg");
-    int i = 0;
-    Blur(img);
-    img.saveImage("blur.jpg");
+void Blur(Image& img) {
+    int width = img.width;
+    int height = img.height;
 
-    return 0;
+    vector<vector<int>> prefixSumR, prefixSumG, prefixSumB;
+    summed_table(img, prefixSumR, prefixSumG, prefixSumB);
+
+    int radius = 21; // Blur radius
+    int area = (2 * radius + 1) * (2 * radius + 1); // Total pixels in the blur area
+
+    for (int i = radius; i < width - radius; i++) {
+        for (int j = radius; j < height - radius; j++) {
+            // Check if the blur region is within the bounds of the image
+            if (i + radius < width && j + radius < height && i - radius - 1 >= 0 && j - radius - 1 >= 0) {
+                int sumR = prefixSumR[i + radius][j + radius] - prefixSumR[i - radius - 1][j + radius] - prefixSumR[i + radius][j - radius - 1] + prefixSumR[i - radius - 1][j - radius - 1];
+                int sumG = prefixSumG[i + radius][j + radius] - prefixSumG[i - radius - 1][j + radius] - prefixSumG[i + radius][j - radius - 1] + prefixSumG[i - radius - 1][j - radius - 1];
+                int sumB = prefixSumB[i + radius][j + radius] - prefixSumB[i - radius - 1][j + radius] - prefixSumB[i + radius][j - radius - 1] + prefixSumB[i - radius - 1][j - radius - 1];
+
+                // Calculate average
+                int avgR = sumR / area;
+                int avgG = sumG / area;
+                int avgB = sumB / area;
+
+                img(i, j, 0) = avgR;
+                img(i, j, 1) = avgG;
+                img(i, j, 2) = avgB;
+            }
+        }
+    }
 }
+
+
+
